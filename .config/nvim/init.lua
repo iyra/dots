4d64b508bffcafd7c3aed8b24caeae433ed5235b
iyra <11570232+iyra@users.noreply.github.com>
2025-01-26 14:31:28 +0000

    vim changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,36 +23,21 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 
 -- color theme
 require("config.lazy")
-vim.cmd.colorscheme "catppuccin"
+vim.cmd 'set background=light'
+vim.g.tokyonight_style = "day"
+vim.cmd 'colorscheme tokyonight'
 
--- tree
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
+-- set line limit
+vim.opt.colorcolumn = "79"
+
 -- misc loads
-require("neo-tree").setup({
-	filesystem = {
-		follow_current_file = {
-			enabled = true,
-			leave_dirs_open = true,
-		}
-	},
-        event_handlers = { 
-         { -- disable line numbering in neo-tree
-             event = "vim_buffer_enter", 
-             handler = function() 
-                 if vim.bo.filetype == "neo-tree" then 
-                     vim.cmd("setlocal nonumber") 
-                 end 
-             end, 
-         },
-        }
-})
 require('lualine').setup({
-        extensions = {'neo-tree'}, 
+        options = {
+                theme = "tokyonight-day"
+        },
 	sections = {
 	    lualine_a = {'mode'},
 	    lualine_b = {'branch', 'diff', 'diagnostics'},
@@ -73,8 +58,6 @@ cmp.setup({
                 cmp.select_next_item()
               elseif vim.fn["vsnip#available"](1) == 1 then
                 feedkey("<Plug>(vsnip-expand-or-jump)", "")
-              elseif has_words_before() then
-                cmp.complete()
               else
                 fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
               end
@@ -96,21 +79,39 @@ cmp.setup({
         snippet = {
         -- REQUIRED - you must specify a snippet engine
                 expand = function(args)
-                        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                        vim.snippet.expand(args.body)
                 end,
         },
         sources = {
     { name = 'path' },                              -- file paths
     { name = 'nvim_lsp', keyword_length = 2},      -- from language server
     { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
-    { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
     { name = 'buffer', keyword_length = 2 },        -- source current buffer
     { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip
     { name = "crates" }
         }
 })
-require('lspconfig').pyright.setup{}
 require('lspconfig').emmet_language_server.setup({})
+
+-- LSP window borders
+local _border = "single"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = _border
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = _border
+  }
+)
+
+vim.diagnostic.config{
+  float={border=_border}
+}
+
 require('crates').setup({
         lsp = {
                 enabled = true,
@@ -125,29 +126,44 @@ require('crates').setup({
         },
 })
 
--- start neo-tree on startup
-vim.api.nvim_create_autocmd("VimEnter", {
-      command = "set nornu nonu | Neotree toggle",
-    })
-vim.api.nvim_create_autocmd("BufEnter", {
-command = "set rnu nu",
-    })
-
 -- keymap
--- file browser
-vim.keymap.set("n", "<space>fr", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
-
 -- telescope general
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', ":Telescope find_files no_ignore=true hidden=true<CR>")
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>bf', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fb', ":Telescope file_browser no_ignore=true hidden=true path=%:p:h select_buffer=true<CR>")
+vim.keymap.set("n", "<leader>gr", "<cmd>Telescope lsp_references<CR>")
+vim.keymap.set("n", "<leader>gde", vim.lsp.buf.declaration)
+vim.keymap.set("n", "<leader>gdf", "<cmd>Telescope lsp_definitions<CR>")
+vim.keymap.set("n", "<leader>gi", "<cmd>Telescope lsp_implementations<CR>")
+vim.keymap.set("n", "<leader>gt", "<cmd>Telescope lsp_type_definitions<CR>")
+vim.keymap.set({ "n", "v" }, "<leader>gca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>grn", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>gea", "<cmd>Telescope diagnostics bufnr=0<CR>")
+vim.keymap.set("n", "<leader>gE", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>gl", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<leader>gh", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>g\\", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>gq", ":LspRestart<CR>")
+
 
 vim.diagnostic.config({
   virtual_text = false
 })
 
--- Show line diagnostics automatically in hover window
-vim.o.updatetime = 250
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+require("mason").setup()
+local dap = require("dap")
+dap.adapters.codelldb = {
+  type = 'server',
+  port = "${port}",
+  executable = {
+    -- CHANGE THIS to your path!
+    command = '/usr/bin/codelldb',
+    args = {"--port", "${port}"},
+
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+  }
+}
--- .config/nvim/lua/plugins/rust-lsp.lua
+++ .config/nvim/lua/plugins/rust-lsp.lua
@@ -3,4 +3,12 @@ return {
   {'mrcjkb/rustaceanvim',
   version = '^5', -- Recommended
   lazy = false}, -- This plugin is already lazy
+  {
+    'dgagn/diagflow.nvim',
+    -- event = 'LspAttach', This is what I use personnally and it works great
+    opts = {
+        scope = 'line',
+        max_height = 15
+    }
+  }
 }
